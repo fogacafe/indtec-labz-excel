0471083e478e40c529e7a648efb5b5a0116e0dad
Felipe Fogaça <felipe.foogaca@gmail.com>
2026-09-01 14:20:10 -0300

    feat: add common Excel format masks

---
+++ src/Indtec.ExcelMapper/Styling/ExcelFormats.cs
@@ -0,0 +1,15 @@
+namespace Indtec.ExcelMapper.Styling;
+
+public static class ExcelFormats
+{
+    public const string DateBrazil = "dd/mm/yyyy";
+    public const string DateIso = "yyyy-mm-dd";
+    public const string DateTimeBrazil = "dd/mm/yyyy hh:mm:ss";
+    public const string DateTimeIso = "yyyy-mm-dd hh:mm:ss";
+    public const string Integer = "0";
+    public const string Decimal2 = "#,##0.00";
+    public const string Decimal4 = "#,##0.0000";
+    public const string Percentage2 = "0.00%";
+    public const string CurrencyBrazil = "R$ #,##0.00";
+    public const string CurrencyUs = "$#,##0.00";
+}
