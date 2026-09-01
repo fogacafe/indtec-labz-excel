eafc85565dbe79bd5a8420d6b0c53711b3dca860
Felipe Fogaça <felipe.foogaca@gmail.com>
2026-09-01 08:27:01 -0300

    feat: add column mapping attribute

---
+++ src/Indtec.ExcelMapper/Attributes/ExcelColumnAttribute.cs
@@ -0,0 +1,15 @@
+namespace Indtec.ExcelMapper;
+
+[AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = true)]
+public sealed class ExcelColumnAttribute : Attribute
+{
+    public ExcelColumnAttribute(string header)
+    {
+        Header = string.IsNullOrWhiteSpace(header)
+            ? throw new ArgumentException("Column header cannot be empty.", nameof(header))
+            : header;
+    }
+
+    public string Header { get; }
+    public int Order { get; set; } = int.MaxValue;
+}
