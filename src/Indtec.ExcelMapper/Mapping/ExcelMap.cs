1c83a2840d6b410d761322864e3137e7b5944247
Felipe Fogaça <felipe.foogaca@gmail.com>
2026-09-01 08:27:10 -0300

    feat: add generated map contracts

---
+++ src/Indtec.ExcelMapper/Mapping/ExcelMap.cs
@@ -0,0 +1,44 @@
+using System.ComponentModel;
+
+namespace Indtec.ExcelMapper.Mapping;
+
+public sealed class ExcelColumnMap
+{
+    public ExcelColumnMap(
+        string header,
+        int order,
+        Type valueType,
+        Func<object, object?> getter,
+        Action<object, object?>? setter)
+    {
+        Header = header;
+        Order = order;
+        ValueType = valueType;
+        Getter = getter;
+        Setter = setter;
+    }
+
+    public string Header { get; }
+    public int Order { get; }
+    public Type ValueType { get; }
+    public Func<object, object?> Getter { get; }
+    public Action<object, object?>? Setter { get; }
+}
+
+public sealed class ExcelTypeMap
+{
+    public ExcelTypeMap(string sheetName, IReadOnlyList<ExcelColumnMap> columns)
+    {
+        SheetName = sheetName;
+        Columns = columns.OrderBy(x => x.Order).ToArray();
+    }
+
+    public string SheetName { get; }
+    public IReadOnlyList<ExcelColumnMap> Columns { get; }
+}
+
+[EditorBrowsable(EditorBrowsableState.Never)]
+public interface IGeneratedExcelMapProvider
+{
+    ExcelTypeMap ExcelMap { get; }
+}
